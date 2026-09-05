f1365049363c3bec53827f55a39592f196712d4d
jcbondsii <jcbondsii@gmail.com>
2026-09-05 16:54:01 -0500

    Added a cfg option for easier calls.

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {
-			break
-		}
-		input := scanner.Text()
-		if input == "exit" {
-			break
-		}
-		cleanedInput := cleanInput(input)
-		fmt.Println("Your command was:", cleanedInput[0])
+	cfg := &config{
+		commands: getCommands(),
 	}
+	startRepl(cfg)
 }
--- repl.go
+++ repl.go
@@ -1,11 +1,69 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !reader.Scan() {
+			break
+		}
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := cfg.commands[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+type config struct {
+	// Add any configuration fields you need here
+	commands map[string]cliCommand
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
